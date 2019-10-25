27035f16aa286b9a0fef0e59a5508b4d9bcee0c6
caioluis <ocaioluis@gmail.com>
2019-10-25 21:22:49 +0100

    Week 5

---
+++ Week 5/week5-088.Students/src/Main.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Student> list = new ArrayList<Student>();
+        
+        
+        while (true) {
+            System.out.println("name: ");
+            String name = reader.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+            System.out.println("studentnumber: ");
+            String studentNumber = reader.nextLine();
+            list.add(new Student(name, studentNumber));
+        }
+        
+        for (Student student: list) {
+            System.out.println(student);
+        }
+
+        System.out.println("Give search term: ");
+        String searchTerm = reader.nextLine();
+        System.out.println("Result: ");
+        for (Student student : list) {
+            if (student.getName().contains(searchTerm)) {
+                System.out.println(student);
+            }
+        }
+    }
+}
