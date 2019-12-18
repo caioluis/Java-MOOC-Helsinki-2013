cb155c0c8ec16bf44b0c0f7d6e0c6b18538ac996
caioluis <ocaioluis@gmail.com>
2019-12-18 20:59:40 +0000

    Week 6
    Week 6

---
+++ Week 6/week6-102.GradeDistribution/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       GradeDistribution grades = new GradeDistribution();
+       grades.score();
+       grades.printGrades();
+       grades.printAcceptance();
+    }
+}
