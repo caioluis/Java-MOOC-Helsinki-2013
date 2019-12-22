056edc2c57c4197614c0c6acfef8effc7c79ea6d
caioluis <ocaioluis@gmail.com>
2019-12-22 16:45:42 +0000

    Update Smileys.java

--- Part 2/Week 7/week7-week7_01.Smileys/src/Smileys.java
+++ Part 2/Week 7/week7-week7_01.Smileys/src/Smileys.java
@@ -1,19 +1,35 @@
-
 public class Smileys {
 
     public static void main(String[] args) {
         // Test your method at least with the following
-        printWithSmileys("87.");
+        printWithSmileys("Mikael");
         // printWithSmileys("Beerbottle");
         // printWithSmileys("Interface");
 
     }
 
     private static void printWithSmileys(String characterString) {
-        for (int i = 0; i <= characterString.length() * 2; i=i+2) {
-               System.out.print(":)");
+        String secondLine = secondLine(characterString);
+
+        lineOfSmileys(secondLine.length() / 2);
+        System.out.println(secondLine);
+        lineOfSmileys(secondLine.length() / 2);
+
+    }
+
+    private static void lineOfSmileys(int length) {
+        for (int i = 0; i < length; i++) {
+            System.out.print(":)");
         }
-        
+        System.out.println("");
+    }
+
+    private static String secondLine(String characterString) {
+        if (characterString.length() % 2 == 1) {
+            return ":) " + characterString + "  :)";
+        }
+        return ":) " + characterString + " :)";
+
     }
 
 }
