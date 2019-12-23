983abf5a4ecabb7d231803545616ecfbe92bd904
caioluis <ocaioluis@gmail.com>
2019-12-23 21:26:45 +0000

    Classes and objects which contain other objects.

---
+++ Part 2/Week 7/week7-week7_04.ThingSuitcaseAndContainer/src/Container.java
@@ -0,0 +1,47 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Caio
+ */
+public class Container {
+    
+    private int totalWeight;
+    private int maximumWeight;
+    private ArrayList<Suitcase> suitcases;
+    
+    public Container(int maximumWeight){
+        this.suitcases = new ArrayList<Suitcase>();
+        this.maximumWeight = maximumWeight;
+    }
+    
+    public void addSuitcase(Suitcase suitcase){
+        if ((this.totalWeight + suitcase.totalWeight()) <= maximumWeight) {
+            suitcases.add(suitcase);
+            totalWeight+=suitcase.totalWeight();
+        }
+    }
+    
+    public String toString(){
+        if (suitcases.isEmpty()) {
+            return "Container is empty (0 kg)";
+        } else if (suitcases.size() == 1){
+            return "1 suitcase ("  + totalWeight + " kg)";
+        } else {
+            return suitcases.size() + " suitcases (" +  totalWeight + " kg)";
+        }
+    }
+    
+     public void printThings(){
+         for (Suitcase suitcase : suitcases) {
+             suitcase.toString();
+         }
+     }
+}
---
+++ Part 2/Week 7/week7-week7_04.ThingSuitcaseAndContainer/src/Suitcase.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Caio
+ */
+import java.util.*;
+public class Suitcase {
+    
+    private int totalWeight;
+    private int maximumWeight;
+    private ArrayList<Thing> things = new ArrayList<Thing>();
+            
+    public Suitcase(int maximumWeight){
+        this.maximumWeight = maximumWeight;
+    }
+    public void addThing(Thing thing){
+        if ((thing.getWeight() + totalWeight) <= maximumWeight) {
+            things.add(thing);
+            totalWeight+=thing.getWeight();
+        }
+        
+    }
+    
+    public String toString(){
+        if (things.isEmpty()) {
+            return "empty (0 kg)";
+        } else if (things.size() == 1) {
+        return things.size() + " thing(" + totalWeight + " kg)"; 
+        } else {
+           return things.size() + " things(" + totalWeight + " kg)";  
+        }
+    }
+    
+    public void printThings(){
+        for (Thing thing: things) {
+            System.out.println(thing);
+        }
+    }
+    
+    public int totalWeight(){
+        return totalWeight;
+    }
+    
+    public Thing heaviestThing(){
+        int heaviestIndex = 0;
+        for (int i = 1; i < things.size(); i++) {
+            if (things.get(i).getWeight() > things.get(i - 1).getWeight()) {
+                heaviestIndex = i;
+            }
+        }
+        
+        return things.get(heaviestIndex);
+    }
+}
