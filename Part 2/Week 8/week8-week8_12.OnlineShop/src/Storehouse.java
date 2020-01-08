00da04d96cbf16b2e25d8f39cf841a5917ec2b01
caioluis <ocaioluis@gmail.com>
2020-01-08 20:59:50 +0000

    First parte of Online Shop.

---
+++ Part 2/Week 8/week8-week8_12.OnlineShop/src/Storehouse.java
@@ -0,0 +1,62 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Caio
+ */
+import java.util.*;
+
+public class Storehouse {
+
+    private String product;
+    private int price;
+    private int stock;
+    private Map<String, Integer> things;
+    private Map<String, Integer> stocks;
+
+    public Storehouse() {
+        things = new HashMap<String, Integer>();
+        stocks = new HashMap<String, Integer>();
+    }
+
+    public void addProduct(String product, int price, int stock) {
+        things.put(product, price);
+        stocks.put(product, stock);
+    }
+
+    public int price(String product) {
+        if (things.containsKey(product)) {
+            return things.get(product);
+        } else {
+            return -99;
+        }
+    }
+
+    public int stock(String product) {
+        if (stocks.containsKey(product)) {
+            return stocks.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product) {
+        if (stocks.containsKey(product)) {
+            if (stocks.get(product) > 0) {
+                stocks.put(product, stocks.get(product) - 1);
+                return true;
+            }
+            
+        } 
+            return false;
+    }
+    
+    //to be continued
+    public Set<String> products(){
+        return null;
+    }
+}
