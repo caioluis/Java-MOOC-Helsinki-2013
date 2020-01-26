b2f21b1a49b471d43a0558f3c0b2814c378a9ac8
caio <ocaioluis@gmail.com>
2020-01-26 15:29:06 +0000

    Organising Classes into Packages

---
+++ Part 2/Week 9/week9-week9_17.FirstPackages/src/mooc/application/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mooc.application;
+
+import mooc.logic.ApplicationLogic;
+import mooc.ui.TextUserInterface;
+import mooc.ui.UserInterface;
+
+/**
+ *
+ * @author Caio
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        UserInterface ui = new TextUserInterface();
+
+        new ApplicationLogic(ui).execute(3);
+
+    }
+}
